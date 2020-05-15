8c0374455e2435d3f8ad15e4eda992b4d4c9aea5
mukesh bhatia <mukeshbhatia@mukeshs-MacBook-Pro.local>
2020-05-15 16:37:37 +0530

    graph issue fix

--- src/components/GraphView.vue
+++ src/components/GraphView.vue
@@ -204,7 +204,7 @@
 
 <style>
   .card-wrap {
-    height: 360px;
+    height: 385px;
     border-radius: 5px;
     box-shadow: 0 1.5px 3px 0 rgba(0, 0, 0, 0.16);
     background-color: #252834 !important;
